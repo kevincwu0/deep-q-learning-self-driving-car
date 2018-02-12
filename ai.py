3845aab31d1f7883ba492fa5e0db8fadcdad48c0
kevincwu0 <kevincwu0@gmail.com>
2018-02-12 14:07:14 -0800

    Creating the architecture of the Neural Network

--- ai.py
+++ ai.py
@@ -1,8 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb 10 12:46:42 2018
+# AI for Self Driving Car
 
-@author: kevin
-"""
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
 
+# Creating the architecture of the Neural network
+
+class Nework(nn.module):
+    def __init__(self, input.size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_values
