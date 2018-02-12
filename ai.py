3a0f0b3bfe7b1c7f5f60937e9f15311ba0c5f114
kevincwu0 <kevincwu0@gmail.com>
2018-02-12 14:10:53 -0800

    Add experience replay

--- ai.py
+++ ai.py
@@ -11,15 +11,31 @@ import torch.autograd as autograd
 from torch.autograd import Variable
 
 # Creating the architecture of the Neural network
-
 class Nework(nn.module):
+
     def __init__(self, input.size, nb_action):
         super(Network, self).__init__()
         self.input_size = input_size
         self.nb_action = nb_action
         self.fc1 = nn.Linear(input_size, 30)
         self.fc2 = nn.Linear(30, nb_action)
+
     def forward(self, state):
         x = F.relu(self.fc1(state))
         q_values = self.fc2(x)
         return q_values
+
+# Implementing Experience Replay
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+
+    def push(self, event):
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+
+    def sample(self, batch_size):
+        samples = zip(*random.sample(self.memory, batch_size))
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
